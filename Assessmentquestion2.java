4d3d89dc511efb0fe8d8416ee10f8ca81711b727
Chukka Venkata Ravi <102050741+Chukkaravi@users.noreply.github.com>
2023-01-15 09:27:04 +0530

    Add files via upload

---
+++ Assessmentquestion2.java
@@ -0,0 +1,10 @@
+//.Write a Java program that takes two numbers as input and display the product of two numbers.
+
+public class Assessmentquestion2{
+    public static void main(String[] args){
+        int a = 30;
+        int b = 2;
+        int c = a * b;
+        System.out.println("Product of the two numbers: "+ c);
+    }
+}
